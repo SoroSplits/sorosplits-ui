2fb0f192cac9a8b87ed95fef32c3353c790f8275
findolor <anakisci@gmail.com>
2023-09-18 12:05:07 +0300

    Add a test page for testing wallet connection

---
+++ pages/test.tsx
@@ -0,0 +1,33 @@
+import { isAllowed, setAllowed, getUserInfo } from '@stellar/freighter-api'
+import { useEffect } from 'react'
+
+
+
+export default function Test() {
+
+
+    useEffect(() => {
+        const setup = async () => {
+            const allowed = await isAllowed()
+            console.log(allowed)
+
+            if (allowed) {
+                const info = await getUserInfo()
+                console.log(info)
+            }
+        }
+
+        setup()
+    }, [])
+
+    const connect = async () => {
+        await setAllowed()
+    }
+
+
+  return (
+    <>
+        <button onClick={connect} >Connect</button>
+    </>
+  )
+}
