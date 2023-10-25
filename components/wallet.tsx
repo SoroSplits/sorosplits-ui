e003f92af57c9a1f44875ab4e0c86127f5cffb2a
findolor <anakisci@gmail.com>
2023-10-25 21:49:23 +0300

    Refactor wallet component

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -1,16 +1,17 @@
 import { useMemo } from "react"
 import Button from "./button"
+import { FiLogOut } from 'react-icons/fi'
 
 interface WalletProps {
   isConnected: boolean
   walletAddress: string
-  connect: () => void
+  toggleButton: () => void
 }
 
-const Wallet = ({ isConnected, walletAddress, connect }: WalletProps) => {
+const Wallet = ({ isConnected, walletAddress, toggleButton }: WalletProps) => {
   const displayFirstLastChars = useMemo(() => {
-    const firstChars = walletAddress.slice(0, 6)
-    const lastChars = walletAddress.slice(-6)
+    const firstChars = walletAddress.slice(0, 4)
+    const lastChars = walletAddress.slice(-4)
 
     return `${firstChars}.....${lastChars}`
   }, [walletAddress])
@@ -18,11 +19,16 @@ const Wallet = ({ isConnected, walletAddress, connect }: WalletProps) => {
   return (
     <>
       {isConnected ? (
-        <div className="w-40 h-10 flex items-center justify-center py-2 px-4 rounded-lg">
-          {displayFirstLastChars}
+        <div className="flex items-center gap-2">
+          <div className="w-[140px] h-10 flex items-center justify-center py-2 px-4 rounded-lg text-sm border-2 border-accent border-opacity-10 cursor-pointer opacity-90 hover:bg-background-dark">
+            {displayFirstLastChars}
+          </div>
+          <button onClick={toggleButton}>
+            <FiLogOut color="black" size={14} />
+          </button>  
         </div>
       ) : (
-        <Button text="Connect Wallet" onClick={connect} type="wallet" />
+        <Button text="Connect Wallet" onClick={toggleButton} type="wallet" />
       )}
     </>
   )
