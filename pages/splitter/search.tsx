8eba0026ed7eac5b4372293cc6ec10e27332a827
findolor <anakisci@gmail.com>
2023-10-25 19:16:40 +0300

    Add search page

--- pages/splitter/search.tsx
+++ pages/splitter/search.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+import { debounce } from "../../hooks/debounce"
+
 export default function SearchSplitter() {
+  const [contractAddress, setContractAddress] = useState("")
+
+  const searchContract = debounce(async (searchTerm: string) => {}, 1000)
+
   return (
     <div>
       <h1 className="text-5xl">Search Splitter</h1>
+
+      <br />
+
+      <input
+        className="w-96 h-10 flex items-center justify-center py-2 px-4 rounded-lg border-[1px] border-[#d2d2d2]"
+        type="text"
+        onChange={(e) => searchContract(e.target.value)}
+        placeholder="Splitter address"
+      />
     </div>
   )
 }
