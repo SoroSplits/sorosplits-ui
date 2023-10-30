d1a1607bdeaf90e713d7ffb705455677d6d03b22
findolor <anakisci@gmail.com>
2023-10-30 11:30:27 +0300

    Display total shares under the data list

--- components/SplitterData.tsx
+++ components/SplitterData.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import Input from "./Input"
 import { CgClose } from "react-icons/cg"
 import { AiOutlineUserAdd } from "react-icons/ai"
@@ -64,8 +64,15 @@ const SplitterData = ({
     updateData(newData)
   }
 
+  const totalShareAmount = useMemo(() => {
+    return data.reduce(
+      (total, item) => total + (isNaN(item.share) ? 0 : item.share),
+      0
+    )
+  }, [data])
+
   return (
-    <div>
+    <div className="w-fit">
       <div className="flex flex-col gap-3">
         {data.map((item, idx) => {
           return (
@@ -101,17 +108,29 @@ const SplitterData = ({
         })}
       </div>
 
-      <button
-        className={clsx(
-          "flex items-center justify-between px-4 py-1 text-sm w-[110px] bg-secondary hover:bg-secondary-dark mt-4 rounded-md",
-          locked && "opacity-50"
-        )}
-        onClick={addData}
-        disabled={locked}
-      >
-        <AiOutlineUserAdd size={16} />
-        Add User
-      </button>
+      <div className="flex justify-between items-end">
+        <button
+          className={clsx(
+            "flex items-center justify-between px-4 py-1 text-sm w-[110px] bg-secondary hover:bg-secondary-dark mt-4 rounded-md",
+            locked && "opacity-50"
+          )}
+          onClick={addData}
+          disabled={locked}
+        >
+          <AiOutlineUserAdd size={16} />
+          Add User
+        </button>
+        <div
+          className={clsx(
+            "py-1 mr-3 font-bold",
+            totalShareAmount > 100 && "text-red-500",
+            totalShareAmount === 100 && "text-green-500",
+            totalShareAmount < 100 && "text-yellow-500"
+          )}
+        >
+          Total: {totalShareAmount}%
+        </div>
+      </div>
     </div>
   )
 }
